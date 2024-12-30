f5b0701eca64eb68e9234866ff2b0144b6ce5fd8
JohnStrandt <jstrandt@hotmail.com>
2024-12-30 14:17:32 -0600

    PWA configured

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,31 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Golf Games",
+        short_name: "Wolf",
+        description: "A collection of popular golf wager games",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "./public/favicons/pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "./public/favicons/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+});
